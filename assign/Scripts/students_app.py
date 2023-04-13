40ed46b83989d44888112ad19eacea6d06bb1389
Divvela-Manikanta <manikanta.divvela124@gmail.com>
2023-04-13 17:12:19 +0530

    modified

--- assign/Scripts/students_app.py
+++ assign/Scripts/students_app.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 from flask import Flask,jsonify,request
-
+import json
 
 app = Flask(__name__)
 client  = MongoClient('localhost',27017)
@@ -13,9 +13,9 @@ def into():
              To insert data you to go for /insert (post method),
              To find data you to go for /find'
              To delete data you to go for /delete'''
-    return list1
+    return jsonify(list1)
     
-@app.route("/view") # to view the entire student data 
+@app.route("/view/") # to view the entire student data 
 def student_view():
     x = collection.find()
     document = []
@@ -25,49 +25,51 @@ def student_view():
     if len(document) !=0:
         return jsonify(document)
     else:
-        return 'Their is no data to view'
+        return jsonify('Their is no data to view')
 
 
 @app.route('/insert',methods =['POST']) # to insert the  student data 
 def insert():
     try:
         val = (request.get_data())
-        val = val.decode('utf-8')
-        val = val.split(',')
-        dict_insert = { "Name":val[0],
-                        "Roll":val[1],
-                        "class":val[2]
+        val = (val.decode('utf-8'))
+        val = json.loads(val)
+        dict_insert = { "Name":val['Name'],
+                        "Roll":val['Roll'],
+                        "class":val['class']
                         }
         dd = collection.insert_one(dict_insert)
         if dd.acknowledged:
-            return "Data is inserted successfully"
+            return jsonify("Data is inserted successfully")
         else:
-            return "Data is not inserted successfully"
+            return jsonify("Data is not inserted successfully")
     
     except Exception as ex:
-        return 'The format of data that you are inserted mightbe wrong'
+        return jsonify('The format of data that you are inserted mightbe wrong')
 
-@app.route('/find',methods =['GET']) # to find the sngle student data 
-def find():
-    data = (request.get_data())
-    data = data.decode('utf-8')
+@app.route('/find/<data>') # to find the sngle student data 
+def find(data):
     val = collection.find_one({'Roll':data})
     if val != None:
         del val['_id']
         return jsonify(val)
     else:
-        return f"The student with roll number {data} is not found in data base"
+        return jsonify(f"The student with roll number {data} is not found in data base")
 
-@app.route('/delete') # to delete the entire student data 
+@app.route('/delete',methods =['POST']) # to delete the entire student data 
 def delete():
-    roll = (request.get_data())
-    roll = roll.decode('utf-8')
-    val = collection.find_one({'Roll':roll})
-    if val != None:
-        collection.delete_one({'Roll':roll})
-        return f"The student info with roll number {roll} is deleted from the data base."
-    else:
-        return f"Unable to datele the student info with roll number {roll}.The student might be not available"
+    try:
+        val = (request.get_data())
+        val = (val.decode('utf-8'))
+        val = json.loads(val)
+        val = collection.find_one({'Roll':val['Roll']})
+        if val != None:
+            collection.delete_one({'Roll':val['Roll']})
+            return jsonify(f"The student info with roll number {val['Roll']} is deleted from the data base.")
+        else:
+            return jsonify(f"Unable to datele the student info with roll number {val['Roll']}.The student might be not available")
+    except Exception as ex:
+        return jsonify("the entred roll number has to be in string format and data has to be gven in the json 'Roll':...")
 
 
 if __name__ =='__main__':
