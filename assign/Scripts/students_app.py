79edc13d9ba744e4b9c2ae923b8f7384939990f3
Divvela-Manikanta <120786207+Divvela-Manikanta@users.noreply.github.com>
2023-04-14 13:03:02 +0530

    Update students_app.py

--- assign/Scripts/students_app.py
+++ assign/Scripts/students_app.py
@@ -3,7 +3,7 @@ from flask import Flask,jsonify,request
 import json
 from dataclasses import dataclass
 from marshmallow import Schema,fields,ValidationError,validate
-from dataclasses import dataclass
+
 
 app = Flask(__name__)
 
